2886fa76e1d9bd20c05ff43ad0ab197e36840e16
CarlosAcosta4 <carlosacosta4820@gmail.com>
2024-03-10 18:57:24 -0500

    Agregado el controlador para realizar tests

---
+++ src/main/java/pe/borabora/controller/TestController.java
@@ -0,0 +1,21 @@
+package pe.borabora.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/accessAdmin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String accessAdmin(){
+        return "Hola, has accedito con rol de ADMIN";
+    }
+
+    @GetMapping("/accessUser")
+    @PreAuthorize("hasRole('USER')")
+    public String accessUser(){
+        return "Hola, has accedito con rol de USER";
+    }
+}
