e62154f5594685cbe20896971a1d6e7b7dfd8286
CarlosAcosta4 <carlosacosta4820@gmail.com>
2024-03-10 16:44:56 -0500

    Primer commit

---
+++ src/main/java/pe/borabora/ApiBoraBoraMovilKotlinApplication.java
@@ -0,0 +1,13 @@
+package pe.borabora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBoraBoraMovilKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBoraBoraMovilKotlinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/borabora/ApiBoraBoraMovilKotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.borabora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBoraBoraMovilKotlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
