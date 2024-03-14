b03f1eba23494a9922ae74704ccf47f5dfd3ddb4
Brigitte <brigitteprietobravo20gmail.com>
2024-03-13 19:33:41 -0500

    aumento de atributos a district y typeorder

--- src/main/java/pe/borabora/entity/District.java
+++ src/main/java/pe/borabora/entity/District.java
@@ -1,4 +1,19 @@
 package pe.borabora.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class District {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cod_district;
+
+    @Column(name = "district", nullable = false)
+    private String district;
 }
--- src/main/java/pe/borabora/entity/TypeOrder.java
+++ src/main/java/pe/borabora/entity/TypeOrder.java
@@ -1,4 +1,23 @@
 package pe.borabora.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class TypeOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_type_order;
+
+    //1:1 delivery
+
+    //1:1 pick up
 }
