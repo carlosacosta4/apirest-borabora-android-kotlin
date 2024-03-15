879d9ce45a28b426684115cc35afbd6535822357
CarlosAcosta4 <carlosacosta4820@gmail.com>
2024-03-14 19:44:40 -0500

    Implementado el secret key y time expiration a application.properties

--- src/main/java/pe/borabora/ApiBoraBoraMovilKotlinApplication.java
+++ src/main/java/pe/borabora/ApiBoraBoraMovilKotlinApplication.java
@@ -2,12 +2,15 @@ package pe.borabora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ApiBoraBoraMovilKotlinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBoraBoraMovilKotlinApplication.class, args);
+		
+		System.out.println(new BCryptPasswordEncoder().encode("1234"));
 	}
 
 }
